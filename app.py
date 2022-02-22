9b5593efee7b767039be6e8c6ea54e559d003048
kiyongee2 <kiyongee2@gmail.com>
2022-02-22 12:51:50 +0900

    게시판

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tbl_member import select_member
 
 app = Flask(__name__)
 
-app.secret_key = "#abcde!"  #비밀키 설정 필수
+app.secret_key = "#abcde!"  #로그인시 에러발생 - 비밀키 설정 필수
 
 # db 접속 함수
 def getconn():
@@ -50,15 +50,22 @@ def register():
         name = request.form['name']
         age = request.form['age']
 
-        # db 저장
+        # db 저장(회원 가입)
         conn = getconn()
         cur = conn.cursor()
         sql = "INSERT INTO member(mid, passwd, name, age) " \
               "VALUES ('%s', '%s', '%s', '%s')" % (id, pwd, name, age)
         cur.execute(sql)
         conn.commit()
+
+        # 자동 로그인
+        sql = "SELECT * FROM member WHERE mid = '%s' AND passwd = '%s'" % (id, pwd)
+        cur.execute(sql)
+        rs = cur.fetchone()
         conn.close()
-        return redirect(url_for('memberlist'))  # redirect - 페이지 이동 함수
+        if rs:
+            session['userID'] = rs[0]   # 회원 가입시 세션 발급
+            return redirect(url_for('memberlist'))
     else:
         return render_template('register.html')
 
@@ -102,5 +109,55 @@ def member_del(id):
     conn.commit()
     conn.close()
     return redirect(url_for('memberlist'))
+
+# 회원 수정
+@app.route('/meber_edit/<string:id>/', methods=['GET', 'POST'])
+def member_edit(id):
+   if request.method == "POST":
+       # 데이터 수집
+       mid = request.form['mid']
+       pwd = request.form['passwd']
+       name = request.form['name']
+       age = request.form['age']
+
+       # db 연동
+       conn = getconn()
+       cur = conn.cursor()
+       sql = "UPDATE member SET passwd='%s', name='%s', age='%s' " \
+             "WHERE mid='%s'" % (pwd, name, age, mid)
+       cur.execute(sql)
+       conn.commit()
+       conn.close()
+       return redirect(url_for('member_view', id=id))  # 해당 id로 경로 설정
+   else:
+       conn = getconn()
+       cur = conn.cursor()
+       sql = "SELECT * FROM member WHERE mid = '%s'" % (id)  #해당 경로 id
+       cur.execute(sql)
+       rs = cur.fetchone()  # 수정할 회원 정보
+       conn.close()
+       return render_template('member_edit.html', rs = rs)
+
+# 게시글 목록
+@app.route('/boardlist/')
+def boardlist():
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board ORDER BY bno DESC"
+    cur.execute(sql)
+    rs = cur.fetchall()
+    conn.close()
+    return render_template('boardlist.html', rs = rs)
+
+# 게시글 상세 페이지
+@app.route('/board_view/<int:id>/')
+def board_view(id):
+    conn = getconn()
+    cur = conn.cursor()
+    sql = "SELECT * FROM board WHERE bno = '%s'" % (id)
+    cur.execute(sql)
+    rs = cur.fetchone()
+    conn.close()
+    return render_template('board_view.html', rs=rs)
 
 app.run(debug=True)
